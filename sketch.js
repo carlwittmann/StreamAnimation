8f5dcaa57c4d93b40f510940dd4ea857675a3701
carlwittmann <113217094+carlwittmann@users.noreply.github.com>
2023-04-25 19:48:13 -0700

    Final edits

--- sketch.js
+++ sketch.js
@@ -1,42 +1,41 @@
 var map;
-var currentIndex = 21; // start with file 21
-var maxIndex = 82; // end with file 82
-var polylineGroup = L.layerGroup(); // create a new layer group
-var loadedPolylines = []; // keep track of loaded polylines
+var currentIndex = 21; // This is the highest elevation and the starting value for the index 
+var maxIndex = 82; // This is the lowest elevation and the final value for the index
+var polylineGroup = L.layerGroup(); // Creating a single layer group containing all the geojson files to speed up rendering
+var loadedPolylines = []; // This is the elevation value that is currently loaded
 
+// Boilerplate setup stuff + preloading GeoJSON data
 function setup() {
-  map = L.map('mapid').setView([49.43687, -123.05016], 11);
-
+  map = L.map('mapid').setView([49.43687, -123.05016], 11); 
   L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
     maxZoom: 18,
   }).addTo(map);
 
+  
   loadGeoJSON();
 }
 
+// This loads the appropriate GeoJSON file as the index incrementally loads each group of line segments
+// Also a bit of line styling.
 function loadGeoJSON() {
   loadJSON('stream_geojsons/' + currentIndex + '.geojson', function(data) {
-    // create a new polyline from the loaded data
     var polyline = L.geoJSON(data, {
       style: function(feature) {
         return {
           weight: 1,
-          color: '#0099ff'
+          color: '#84dcc6'
         };
       }
     });
-
-    // add the new polyline to the layer group
+ 
+// Adding the currently loaded GeoJSON to the layer group
     polylineGroup.addLayer(polyline);
-
-    // add the new polyline to the loadedPolylines array
     loadedPolylines.push(polyline);
 
-    // increment the index
+// Index function for incrementing through the folder of GeoJSONs as they are drawn
+// Also clears the drawing once we reach the maximum index value /  lowest elevation
     currentIndex++;
-
-    // if we have reached the end of the files, add the layer group to the map and clear it
     if (currentIndex > maxIndex) {
       polylineGroup.addTo(map);
       currentIndex = 21;
@@ -44,7 +43,7 @@ function loadGeoJSON() {
       polylineGroup.clearLayers();
     }
 
-    // call the loadGeoJSON function again after a delay
+// Speeding up the animation to 10 frames per second / 100 ms per frame
     setTimeout(loadGeoJSON, 100);
   });
 }
